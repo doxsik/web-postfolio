f65abe428dde13b1d9cfb3a83aafd746e231638a
Eugene Bizunok <doxamv@gmail.com>
2022-03-15 21:31:11 +0300

    Commit 15.03

--- blog/models.py
+++ blog/models.py
@@ -5,5 +5,6 @@ class Blog(models.Model):
     date = models.DateField()
     description = models.CharField(max_length=248)
     textar = models.TextField(default='')
+
     def __str__(self):
         return self.title
